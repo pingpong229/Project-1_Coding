4d61a669580faffbbf6ccfcccb6decebc10ac5ee
pingpong229 <pingpingworakamon@gmail.com>
2024-12-04 16:11:04 +0700

    Add files via upload

---
+++ 1form_44pingpong.py
@@ -0,0 +1,8 @@
+print("Welcome to Badminton court")
+print("\n\nสวัสดีค่ะ กรอกชื่อและเวลาที่ยืม")
+name = input("ชื่อ")
+time = input("เวลา")
+quantity = input("จำนวนอุปกรณ์")
+print("\n\nยินดีต้อนรับ คุณ", name)
+print("เวลา ", time, "จำนวนอุปกรณ์", quantity)
+print("\n\nขอบคุณที่มาใช้บริการที่สนามแบตมินตันของเรา")
